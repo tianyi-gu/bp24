8df78c5738b40c74d8c31f5c6980e3c9a9aa56fb
Tianyi <tianyi.evans@gmail.com>
2024-03-03 10:58:30 -0500

    fixing styling

--- bp24-main/background.js
+++
@@ -1,8 +0,0 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.action === 'capture') {
-      chrome.tabs.captureVisibleTab(null, {format: 'png'}, (dataUrl) => {
-        chrome.tabs.create({url: dataUrl});
-      });
-    }
-  });
-  
