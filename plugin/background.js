5aa6d0c9d331219282a1cdb43531192b718144ee
Tianyi <tianyi.evans@gmail.com>
2024-03-03 15:45:14 -0500

    things

--- plugin/background.js
+++ plugin/background.js
@@ -1,6 +1,7 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === "capture") {
     chrome.tabs.captureVisibleTab(null, { format: "png" }, (dataUrl) => {
+      chrome.tabs.sendMessage(sender.tab.id, { screenshotUrl: dataUrl });
       sendResponse({ screenshotUrl: dataUrl });
     });
     return true; // Indicates that the response is asynchronous
