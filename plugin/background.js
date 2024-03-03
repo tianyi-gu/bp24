e97238f5ad0c8860a80c6f46489eff304def1291
Tianyi <tianyi.evans@gmail.com>
2024-03-03 16:13:45 -0500

    fixing buttonsfdasf

--- plugin/background.js
+++ plugin/background.js
@@ -3,7 +3,9 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === "capture") {
     chrome.tabs.captureVisibleTab(null, { format: "png" }, (dataUrl) => {
       console.log("took screenshot 1");
-      chrome.tabs.sendMessage(sender.tab.id, { screenshotUrl: dataUrl });
+      if (sender.tab) {
+        chrome.tabs.sendMessage(sender.tab.id, { screenshotUrl: dataUrl });
+      }
       sendResponse({ screenshotUrl: dataUrl });
       console.log("took screenshot 2");
     });
